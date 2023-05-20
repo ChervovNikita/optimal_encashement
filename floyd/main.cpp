33bcefacfa2146239ef3608f66339bf5c8120e6d
Chervov Nikita <gcnikitachervov@gmail.com>
2023-05-21 01:47:05 +0300

    innitial

---
+++ floyd/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+const int n = 1630;
+
+double a[n][n], w[n][n];
+
+int main() {
+    freopen("data.txt", "r", stdin);
+//    freopen("result.txt", "w", stdout);
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> a[i][j];
+            w[i][j] = a[i][j];
+        }
+    }
+
+    for (int k = 0; k < n; ++k) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                w[i][j] = min(w[i][j], w[i][k] + w[k][j]);
+            }
+        }
+    }
+
+    double sum = 0;
+    double cnt = 0;
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            sum += abs(w[i][j] - a[i][j]);
+            cnt += 1;
+        }
+//        cout << '\n';
+    }
+    cout << sum / cnt << '\n';
+}
