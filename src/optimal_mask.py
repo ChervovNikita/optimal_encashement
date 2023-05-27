1fc151b3752b03a24365c90156becb24e2a51655
Chervov Nikita <gcnikitachervov@gmail.com>
2023-05-27 16:42:55 +0300

    find optimal mask

---
+++ src/optimal_mask.py
@@ -0,0 +1,21 @@
+import os
+
+code_path = './src/optimal_mask/main.cpp'
+script_path = './src/cpp_mask'
+buffer = './data/processed/buffer'
+
+def find_optimal(day, wait, adds):
+    with open(buffer + '_in', 'w') as f:
+        f.write(f'{day} {wait}\n{" ".join([str(add) for add in adds])}\n')
+    with open(buffer + "_out", 'w') as f:
+        f.write("")
+    if not os.path.exists(script_path):
+        os.system(f'g++ {code_path} -o {script_path}')
+    os.system(f'{script_path} < {buffer + "_in"} > {buffer + "_out"}')
+    status, delta = [float(val) for val in open(buffer + "_out", 'r').readline().strip().split(' ')]
+    return int(status), delta
+
+
+if __name__ == '__main__':
+    adds = [160000, 90000, 105000, 99000, 107000, 110000, 60000, 75000, 89000, 95000, 116000, 85000, 110000, 84000, 51000, 51000, 150000, 0, 225000, 75000, 0, 175000, 55000, 125000, 110000, 115000, 100000, 75000, 99000, 100000]
+    print(find_optimal(25, 13, adds))
