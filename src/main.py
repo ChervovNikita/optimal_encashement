21a87cb5fbb13a63abccb1d452e130e86b0a6a64
sergak0 <dog.vol.serg@gmail.com>
2023-05-27 20:40:46 +0300

    add predictor_masks.py and optuna script

--- src/main.py
+++ src/main.py
@@ -1,16 +1,28 @@
 import predictor
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing
+import plotly.express as px
+from tqdm import tqdm
+
+import optuna
+from optuna.samplers import RandomSampler
+from optuna.samplers import TPESampler
+import os
+
+INF = int(1e9)
 
 
-def read_dist():
-    dist = pd.read_csv('../data/raw/times v4.csv')
+def read_dist(dist_path):
+    dist = pd.read_csv(dist_path)
     le = preprocessing.LabelEncoder()
     le.fit(dist['Origin_tid'])
     dist['from_int'] = le.transform(dist['Origin_tid'])
     dist['to_int'] = le.transform(dist['Destination_tid'])
-    dist.head()
+    return dist
 
 
-config = {'cnt_terminals': dist['from_int'].max() + 1,
+config = {'cnt_terminals': 1630,
           'persent_day_income': 0.02 / 365,
           'terminal_service_cost': 100, #{'min': 100, 'persent': 0.01},
           'max_terminal_money': 1000000,
@@ -20,3 +32,36 @@ config = {'cnt_terminals': dist['from_int'].max() + 1,
           'service_time': 10,
           'left_days_coef': 0,
           'encashment_coef': 0}
+
+
+def optimize_configs(trial):
+    config['encashment_coef'] = trial.suggest_int('encashment_coef', 0, 30, step=5, log=False)
+    # config['left_days_coef'] = trial.suggest_int('left_days_coef', 0, 100, step=5, log=False)
+    model = predictor.Predictor(config, dist)
+    try:
+        hist, opt = model.build_ans(data[data.columns[1:]].values, num_days=31, num_vehicles=25,
+                                    vrp_time_limit=30, verbose=False)
+    except:
+        return INF
+
+    return sum(hist['loss'])
+
+
+if __name__ == '__main__':
+    dirname = os.path.dirname(os.path.abspath(__file__))
+    dist = read_dist(os.path.join(dirname, '../data/raw/times v4.csv'))
+    data = pd.read_excel(os.path.join(dirname, '../data/raw/terminal_data_hackathon v4.xlsx'), 'Incomes')
+
+    optuna.logging.set_verbosity(optuna.logging.WARNING)
+
+    study = optuna.create_study(sampler=TPESampler(seed=42,
+                                                   multivariate=True,
+                                                   warn_independent_sampling=False, ), )
+
+    study.optimize(optimize_configs,
+                   n_trials=6,
+                   n_jobs=6,
+                   show_progress_bar=True, )
+
+    print(study.best_value, study.best_params)
+
--- src/optimal_mask.py
+++ src/optimal_mask.py
@@ -2,9 +2,10 @@ import os
 
 dirname = os.path.dirname(os.path.abspath(__file__))
 
-code_path = os.path.join(dirname, 'optimal_mask/main.cpp')
-script_path = os.path.join(dirname, 'cpp_mask')
+code_path = os.path.join(dirname, 'optimal_mask/masks.cpp')
+script_path = os.path.join(dirname, 'optimal_mask/cpp_mask')
 buffer = os.path.join(dirname, '../data/processed/buffer')
+
 
 def find_optimal(day, wait, adds):
     with open(buffer + '_in', 'w') as f:
@@ -20,4 +21,4 @@ def find_optimal(day, wait, adds):
 
 if __name__ == '__main__':
     adds = [160000, 90000, 105000, 99000, 107000, 110000, 60000, 75000, 89000, 95000, 116000, 85000, 110000, 84000, 51000, 51000, 150000, 0, 225000, 75000, 0, 175000, 55000, 125000, 110000, 115000, 100000, 75000, 99000, 100000]
-    print(find_optimal(25, 13, adds))
+    print(find_optimal(20, 13, adds))
